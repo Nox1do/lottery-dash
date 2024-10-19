8a30da2a631f02aa9134816fb6bbc42455dac3b3
Noé G <noogux@gmail.com>
2024-10-18 21:37:03 -0400

    Update app.py CORS Update

--- backend/app.py
+++ backend/app.py
@@ -59,6 +59,12 @@ def get_lottery_results():
         "states_with_results": list(results.keys())
     }
     return jsonify(response)
-
+@app.after_request
+def after_request(response):
+    response.headers.add('Access-Control-Allow-Origin', '*')
+    response.headers.add('Access-Control-Allow-Headers', 'Content-Type,Authorization')
+    response.headers.add('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS')
+    return response
+    
 if __name__ == '__main__':
     app.run(debug=True)
