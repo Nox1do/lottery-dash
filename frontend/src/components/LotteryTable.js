c7b5852554a9b1b515317b7cd18ce0bb22422cc5
Noé G <noogux@gmail.com>
2024-10-20 22:31:02 -0400

    Update LotteryTable.js 2.5

--- frontend/src/components/LotteryTable.js
+++ frontend/src/components/LotteryTable.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState } from 'react'; 
 
 const stateOrder = [
   'tennessee', 'texas', 'maryland', 'ohio', 'georgia', 'new-jersey', 'south-carolina', 'michigan',
